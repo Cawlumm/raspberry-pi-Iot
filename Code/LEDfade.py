b90edac961bf277c717d5611c22d87bc8fbdc726
Carter Lumm <cwlumm@gmail.com>
2023-10-03 18:59:47 -0400

    LEDfade.py

--- Code/LEDfade.py
+++ Code/LEDfade.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-import RPI.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 channel = 11 # GPIO
 
 GPIO.setmode(GPIO.BOARD) # Set which board
 
-GPIO.set(channel, GPIO.OUT) # Set channel and pin method
+GPIO.setup(channel, GPIO.OUT) # Set channel and pin method
 
 # Create a PWM object with a frequency of 100 Hz
 pwm = GPIO.PWM(channel, 100)
@@ -18,11 +18,13 @@ try:
         # increase brightness
         for duty_cycle in range(0, 101, 5):
             pwm.ChangeDutyCycle(duty_cycle)
+            print(f"Increasing intensity: {duty_cycle}")
             time.sleep(0.1)
         
         # decrease brightness
         for duty_cycle in range(100, -1, -5):
             pwm.ChangeDutyCycle(duty_cycle)
+            print(f"Decreasing intensity: {duty_cycle}")
             time.sleep(0.1)
 
 except KeyboardInterrupt:

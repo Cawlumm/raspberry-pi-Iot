e65d21ef27cb3dd6c0262f349af9a3e4c402164f
Carter Lumm <cwlumm@gmail.com>
2023-10-03 18:40:19 -0400

    blink.py

---
+++ Code/blink.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import RPI.GPIO as GPIO
+import time
+
+channel = 11 # GPIO
+
+GPIO.setmode(GPIO.BOARD) # Set which board
+
+GPIO.set(channel, GPIO.OUT) # Set channel and pin method
+
+# Create a PWM object with a frequency of 100 Hz
+pwm = GPIO.PWM(channel, 100)
+pwm.start(0)
+
+try:
+    while True:
+        # increase brightness
+        for duty_cycle in range(0, 101, 5):
+            pwm.ChangeDutyCycle(duty_cycle)
+            time.sleep(0.1)
+        
+        # decrease brightness
+        for duty_cycle in range(100, -1, -5):
+            pwm.ChangeDutyCycle(duty_cycle)
+            time.sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
+finally:
+    pwm.stop()
+    GPIO.cleanup()
